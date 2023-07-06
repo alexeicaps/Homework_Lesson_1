2cb897a17d7aee367b076f90f949794f46027c16
Alexei Capsamun <caps.alex@mail.ru>
2023-07-06 12:29:56 +0300

    Added and resolved task2 and added gitignore

---
+++ task4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Type the number: ");
+int number = int.Parse(Console.ReadLine()!);
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number > number1 & number > number2)
+{
+    Console.Write("max = " + number);
+}
+else if (number1 > number & number1 > number2)
+{
+    Console.Write("max = " + number1);
+}
+else if
+{
+    Console.Write("max = " + number2);
+}
+else
+{
+    Console.Write("Some numbers are equal")
+}
+
+
